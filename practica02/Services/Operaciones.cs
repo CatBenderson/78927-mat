7e28a408b33ae944a847100641cdd4a15e3e578c
CatBenderson <ameyalli030@gmail.com>
2023-02-28 11:02:25 -0600

    28/02/23

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -4,13 +4,20 @@ using WSDL.mensajes;
 namespace WSDL.operaciones{
     
     public class Operaciones: Mensajes{
+        String [] saludados = new String[100];
+        String x="No hallado";
+        int aux=0;
         string Mensajes.Saludar(string nombre){
-            string msj="Hola" + nombre;
+            string msj="Hola " + nombre;
+            saludados[aux]=nombre;
+            aux++;
             return msj;
+
         }
         
         string Mensajes.Mostrar(int id){
-            return "x";
+            x=saludados[id];
+            return x;
         }
     }
 }
