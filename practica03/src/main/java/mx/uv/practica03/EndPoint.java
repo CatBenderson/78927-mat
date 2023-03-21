2ae1ecd3f404588e44c0f79bf18ad28cc190d42d
CatBenderson <ameyalli030@gmail.com>
2023-03-21 11:37:16 -0600

    Servicio saludos

--- practica03/src/main/java/mx/uv/practica03/EndPoint.java
+++ practica03/src/main/java/mx/uv/practica03/EndPoint.java
@@ -1,10 +1,14 @@
 package mx.uv.practica03;
 
+import java.util.ArrayList;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import https.t4is_uv_mx.saludos.BorrarRequest;
+import https.t4is_uv_mx.saludos.BorrarResponse;
 import https.t4is_uv_mx.saludos.ModificarRequest;
 import https.t4is_uv_mx.saludos.ModificarResponse;
 import https.t4is_uv_mx.saludos.PedirResponse;
@@ -13,7 +17,7 @@ import https.t4is_uv_mx.saludos.SaludarResponse;
 
 @Endpoint
 public class EndPoint {
-    String [] nombres = new String[100];
+    ArrayList<String> nombres = new ArrayList<String>();
     int i=0;
 
     @PayloadRoot(localPart = "SaludarRequest", namespace="https://t4is.uv.mx/saludos")
@@ -21,7 +25,7 @@ public class EndPoint {
     @ResponsePayload
     public SaludarResponse Saludar( @RequestPayload  SaludarRequest peticion) {
         SaludarResponse response= new SaludarResponse();
-        nombres[i]=peticion.getNombre();
+        nombres.add(peticion.getNombre());
         i++;
         response.setRespuesta("Hola "+peticion.getNombre());
         return response;
@@ -31,14 +35,18 @@ public class EndPoint {
 
     @ResponsePayload
     public ModificarResponse Modificar( @RequestPayload  ModificarRequest peticion) {
+        int aux=0;
         ModificarResponse response= new ModificarResponse();
-        for (int x = 0; x < nombres.length; x++) {
-            if (peticion.getNombreOld().equals(nombres[x])){
-                nombres[x]=peticion.getNombreNew();
-                response.setRespuesta(peticion.getNombreOld()+" modificado por "+nombres[x]);
-            }else{
-                response.setRespuesta("Valor no encontrado");
+        for (int x = 0; x < nombres.size(); x++) {
+            if (peticion.getNombreOld().equals(nombres.get(x))){
+                nombres.set(x, peticion.getNombreNew());
+                response.setRespuesta(peticion.getNombreOld()+" modificado por "+nombres.get(x));
+                aux=1;
             }
+        }
+        if (aux==0){
+            response.setRespuesta("Valor no encontrado");
+            aux=0;
         }
         return response;
     }
@@ -49,12 +57,32 @@ public class EndPoint {
     public PedirResponse Pedir() {
         PedirResponse response= new PedirResponse();
         String todos="He saludado a: ";
-        for (int x = 0; x < nombres.length; x++) {
-          if(nombres[x]!=null){
-            todos=todos+ nombres[x] +", ";
+        for (int x = 0; x < nombres.size(); x++) {
+          if(nombres.get(x)!=null){
+            todos=todos+ nombres.get(x) +", ";
           }
         }
         response.setRespuesta(todos);
+        return response;
+    }
+
+    @PayloadRoot(localPart = "BorrarRequest", namespace="https://t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public BorrarResponse Borrar( @RequestPayload  BorrarRequest peticion) {
+        BorrarResponse response= new BorrarResponse();
+        int aux=0;
+        for (int x = 0; x < nombres.size(); x++) {
+            if (peticion.getNombre().equals(nombres.get(x))){
+                nombres.remove(x);
+                response.setRespuesta(peticion.getNombre()+" eliminado");
+                aux=1;
+            }
+        }
+        if (aux==0){
+            response.setRespuesta("Valor no encontrado");
+            aux=0;
+        }
         return response;
     }
 }
